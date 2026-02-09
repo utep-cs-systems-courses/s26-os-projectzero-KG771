bfea628919ee84a7a85c61b0cd52c36c69427410
Karen <kgarcia771@outlook.com>
2026-02-08 18:37:57 -0700

    created new function

--- project0.py
+++ project0.py
@@ -9,15 +9,22 @@ def file_reader(input_file):
     info = os.read(fd, 17138)
     print("File reader info:", info)
 
+    os.close(fd)
+
+def extract_text():
+    
+
 def file_writer(output_file, dictionary):
     #output file can be write-only, created, or overwritten (if it exists)
     fd = os.open(output_file, os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
     #print("Output file fd: ", fd)
 
     for key in dictionary:
-        string = f"{key} {dictionary[key]}"
+        string = f"{key} {dictionary[key]}\n"
         strToBytes = string.encode("utf-8")
         os.write(fd, strToBytes)
+
+    os.close(fd)
 
 def word_count(words):
     wordCounts = {}
@@ -26,8 +33,9 @@ def word_count(words):
             wordCounts[word] = 1
         else:
             wordCounts[word] += 1
+    return wordCounts
 
 
 file_reader("declaration.txt")
-file_writer("output.txt")
+file_writer("output.txt", )
 
