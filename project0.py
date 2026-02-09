af26f948d666393aa606f41998fd8206bad4456b
Karen <kgarcia771@outlook.com>
2026-02-08 18:00:25 -0700

    updated

--- project0.py
+++ project0.py
@@ -1,10 +1,29 @@
 import os
 import re
 
-def file_reader(file):
+def file_reader(input_file):
     #input files can be read only (os.O_RDONLY)
-    fd = os.open(file, os.O_RDONLY)
-    #print(fd)
+    fd = os.open(input_file, os.O_RDONLY)
+    #print("Input file fd: ", fd)
+    
+    info = os.read(fd, 17138)
+    print("File reader info:", info)
+
+def file_writer(output_file):
+    #output file can be write-only, created, or overwritten (if it exists)
+    fd = os.open(output_file, os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
+    #print("Output file fd: ", fd)
+    info = os.write(fd, b"hii")
+
+def word_count(words):
+    wordCounts = {}
+    for word in words:
+        if word not in wordCounts:
+            wordCounts[word] = 1
+        else:
+            wordCounts[word] += 1
+
 
 file_reader("declaration.txt")
+file_writer("output.txt")
 
